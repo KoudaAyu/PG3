f7a6ea5172dbffae5cfa67a4aaa141147006b593
AyuKouda <k024g0061@g.neec.ac.jp>
2026-01-26 10:23:50 +0900

    足りない要素の追加

--- 02_01/main.cpp
+++ 02_01/main.cpp
@@ -1,15 +1,27 @@
-#include<stdio.h>
-template<typename T>
+#include <iostream>
+
+// 関数テンプレートを用いて、小さい値を返す関数Min()を作成
+template <typename T>
 T Min(T a, T b)
 {
-	return (a < b) ? a : b;
+    return (a < b) ? a : b;
 }
 
 int main()
 {
-	printf("Min(3, 5) = %d\n", Min(3, 5));
-	printf("Min(3.5f, 2.5f) = %f\n", Min(3.5f, 2.5f));
-	printf("Min(0.5, 2.5) = %f\n", Min(0.5, 2.5));
+    // int型、float型、double型の3対の引数を用意して呼び出す
 
-	return 0;
+    // int型
+    int i1 = 3, i2 = 5;
+    std::cout << "Min(int): " << Min(i1, i2) << std::endl;
+
+    // float型
+    float f1 = 3.5f, f2 = 2.5f;
+    std::cout << "Min(float): " << Min(f1, f2) << std::endl;
+
+    // double型
+    double d1 = 0.5, d2 = 2.5;
+    std::cout << "Min(double): " << Min(d1, d2) << std::endl;
+
+    return 0;
 }
