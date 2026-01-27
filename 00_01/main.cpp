f32ddf6fc2a3f9be2aaf74010e88af66ed9781a6
AyuKouda <k024g0061@g.neec.ac.jp>
2026-01-27 12:45:26 +0900

    printfに変更

--- 00_01/main.cpp
+++ 00_01/main.cpp
@@ -1,10 +1,11 @@
 #include <Windows.h>
-#include <iostream>
+#include <cstdio>
+
 int main()
 {
 	/// Set console output to UTF-8
 	SetConsoleOutputCP(65001);
 	char str[] = "たけのこの里";
-	std::cout << str;
+	printf("%s", str);
 	return 0;
 }
